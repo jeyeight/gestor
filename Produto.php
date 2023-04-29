d7bcd710b9f35d5ea053a4b351a06647e796a433
JoaoP <jvplucas@hotmail.com>
2023-04-28 22:24:24 -0300

    pequenasss mudançasss

--- Produto.php
+++ Produto.php
@@ -1,10 +1,12 @@
 <?php
 require_once('PDO.php');
+print_r($dados);
 class Produto{
+    private $PDO;
     public $id;
 
-    public function listagem(){
-        $sql = $PDO ->query("SELECT * FROM teste.produtos WHERE id = {$id}")
-    }
+    // public function listagem(){
+    //     $sql = $PDO ->query("SELECT * FROM produtos");
+    // }
 }
 ?>
